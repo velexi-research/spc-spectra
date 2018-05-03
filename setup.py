e7ab36596fa448781513fa2972809f909457f2b6
Nicolas Macro <nmacro34@gmail.com>
2018-05-03 15:18:31 -0400

    Prepare for PyPI
    Add setup.cfg and modify setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,13 @@ from distutils.core import setup
 
 import spc
 
-setup(name='spc',
+setup(name='spc-spectra',
       version=spc.__version__,
       description=spc.__doc__,
       author=spc.__author__,
       author_email=spc.__author_email__,
-      url='https://github.com/rohanisaac/spc',
-      packages=['spc'],
-      install_requires=['numpy'],
-      classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        ],
+      url='https://github.com/NickMacro/spc-spectra',
+      download_url='https://github.com/NickMacro/spc-spectra/archive/0.4.0.tar.gz',
+      packages=['spc-spectra'],
+      classifiers=[],
       )
