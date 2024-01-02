fc07f480256e409bf8e45310ae9c7bb4155b5e57
Kevin Chu <kevin@velexi.com>
2024-01-02 12:39:39 -0800

    Remove package metadata from __init__.py.
    - Fix flake8 errors.

--- src/spc_spectra/__init__.py
+++ src/spc_spectra/__init__.py
@@ -2,9 +2,5 @@
 Module for reading, exploring and converting SPC spectroscopic binary data in Python.
 """
 
-from .spc import File
-
-__author__ = "Rohan Isaac"
-__author_email__ = "rohan_isaac@yahoo.com"
-__version__ = "0.4.0"
-__license__ = "GPLv3"
+# Public API
+from .spc import File  # noqa
